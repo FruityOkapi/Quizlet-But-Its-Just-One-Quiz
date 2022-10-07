f7fbadb884c16c325a3b24a8e4e82a3b76fb3fd1
FruityOkapi <josephbsimmerman@gmail.com>
2022-10-06 23:57:07 -0400

    added new functions and buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Sets timer to 0 as default
 var timer = 0;
 // These set query selectors for the various elements in the HTML
 var leadb = $('#leaderB');
@@ -10,30 +11,6 @@ var ctext = $('#ctext');
 var YoN = $('#validate');
 var startQ = $('#startQ');
 var clear = $('#clear');
-
-function startQuiz() {
-
-}
-
-function dispStart() {
-    // This will set the styling of the quiz.
-    quizCard.attr('class', 'qCard')
-    // This will set the text of the initial start of the quiz
-    quizQ.attr('class', 'title');
-    quizQ.text('League of Legends Quiz');
-    ctext.attr('class', 'content');
-    ctext.text('Test your knowledge on all things League. The timer will count down as you take this quiz. Any wrong answers will subtract 8 seconds. Once you reach the end of the quiz or the timer reaches 0, you will be presented with your score and you can save your scores with your initials. Your score is how much time was left. Good luck!');
-    // This sets the text of the top header for leaderboards button and timer
-    timerEl.text('Time left: '+ timer);
-    leadb.text('Leaderboards');
-    // ADD AN EVENT LISTENER WHEN YOU MAKE THE LEADERBOARDS!!!!!!!!!!!!!!!!!!!!!!!!!
-
-    // This is to make the start quiz button
-
-    var startButton = $('<button>');
-    startButton.
-    startButton.click(startQuiz());
-}
 
 // This is to make an array for the 10 questions to be grabbed later
 var qs = [
@@ -70,10 +47,44 @@ var a7 = ['A. Zedd', 'B. Against the Current', 'C. Imagine Dragons', 'D. Nicki T
 var a8 = ['A. 2018', 'B. 2015', 'C. 2016', 'D. 2017']
 
 var a9 = ['A. Silvy', 'B. Shelly', 'C. Shuckle','D. Shirley', "E. Trick question it's B and D cause there are 2 heralds"]
+function startTimer() {
+
+}
+function startQuiz() {
+    startQ.text('')
+
+    quizQ.text(qs[0])
+
+}
+
+function dispStart() {
+    // This will set the styling of the quiz.
+    quizCard.attr('class', 'qCard')
+    // This will set the text of the initial start of the quiz
+    quizQ.attr('class', 'title');
+    quizQ.text('League of Legends Quiz');
+    ctext.attr('class', 'content');
+    ctext.text('Test your knowledge on all things League. The timer will count down as you take this quiz. Any wrong answers will subtract 8 seconds. Once you reach the end of the quiz or the timer reaches 0, you will be presented with your score and you can save your scores with your initials. Your score is how much time was left. Good luck!');
+    // This sets the text of the top header for leaderboards button and timer
+    timerEl.text('Time left: '+ timer);
+    leadb.text('Leaderboards');
+    // ADD AN EVENT LISTENER WHEN YOU MAKE THE LEADERBOARDS!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+    // This is to make the start quiz button
+    var startButton = $('<button>');
+    startButton.text('Start Quiz');
+    startButton.attr('class', 'options');
+    startQ.append(startButton);
+    startQ.click(function() {
+        startQuiz()
+    });
+}
+
+dispStart()
+
 
 
 
 
 // This is to make sure the page displays the startup page
-dispStart()
 
